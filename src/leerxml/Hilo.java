b43ef4b8b6cc555f3866cdd81a4e8eac0eee119f
Hairon Gonzalez Iturriaga <hairon.gonzalez@usach.cl>
2016-01-05 20:38:01 -0300

    Sin StopWords
    Quito las Stopword y las reemplazo por ***
    resultado se entrega en ficheros con la ID de paguina como nombre

--- src/leerxml/Hilo.java
+++ src/leerxml/Hilo.java
@@ -5,10 +5,13 @@
  */
 package leerxml;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
@@ -20,13 +23,14 @@ import java.util.logging.Logger;
  */
 public class Hilo extends Thread{
     //Se declarna las variables a utilizar
-    String Texto=new String(); 
+    String Texto,SinBasura,SinStopWords=new String(); 
+    String StopWords = " algún | alguna | algunas | algun | algunos | ambos | ampleamos | ante | antes | aquel | aquellas | aquellos | aqui | arriba | atras | bajo | bastante | bien | cada | cierta | ciertas | cierto | ciertos | como | con | conseguimos | conseguir | consigo | consigue | consiguen | consigues | cual | cuando | dentro | desde | donde | dos | el | ellas | ellos | empleais | emplean | emplear | empleas | empleo | en | encima | entonces | entre | era | eramos | eran | eras | eres | es | esta | estaba | estado | estais | estamos | estan | estoy | fin | fue | fueron | fui | fuimos | gueno | ha | hace | haceis | hacemos | hacen | hacer | haces | hago | incluso | intenta | intentais | intentamos | intentan | intentar | intentas | intento | ir | la | largo | las | lo | los | mientras | mio | modo | muchos | muy | nos | nosotros | otro | para | pero | podeis | podemos | poder | podria | podriais | podriamos | podrian | podrias | por | por qué | porque | primero | puede | pueden | puedo | quien | sabe | sabeis | sabemos | saben | saber | sabes | ser | si | siendo | sin | sobre | sois | solamente | solo | somos | soy | su | sus | también | teneis | tenemos | tener | tengo | tiempo | tiene | tienen | todo | trabaja | trabajais | trabajamos | trabajan | trabajar | trabajas | trabajo | tras | tuyo | ultimo | un | una | unas | uno | unos | usa | usais | usamos | usan | usar | usas | uso | va | vais | valor | vamos | van | vaya | verdad | verdadera | verdadero | vosotras | vosotros | voy | yo";
     int ID;
     
     public Hilo(String Texto,int ID){ //Los recibe del servidor
 
         //Luego se instancian
-        this.Texto= Texto;
+        this.Texto= " "+Texto;
         this.ID=ID;
         
         
@@ -36,33 +40,42 @@ public class Hilo extends Thread{
     @Override
     public void run() {
      
-      String palabras = Texto.replaceAll("[^A-Za-z0-9-á-úÁ-Ú\n]", " ");
-      
-      System.out.print("Hebra "+ID+": "+palabras+"\n");
-      
-      PrintWriter writer = null;
-        try {
-            writer = new PrintWriter(""+ID+".txt", "UTF-8");
-        } catch (FileNotFoundException | UnsupportedEncodingException ex) {
-            Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        writer.print(palabras);
-        
-        writer.close();
+        EliminarCaracteresInutiles();
+        EliminarStopWords();
+       // ContaryEnviar();
+        Crearfichero();
+                
 
     }
 
-public void ElminarCaracteresInutiles(String texto,int id){
+public void EliminarCaracteresInutiles(){
+    
+    SinBasura = Texto.replaceAll("[^A-Za-z0-9-á-úÁ-Ú]", " ");
+      
+    //System.out.print("Hebra "+ID+": "+SinBasura+"\n");
+    }
+    
+    public void EliminarStopWords(){
         
+        SinStopWords=SinBasura.replaceAll(StopWords, "***");
+    
+    }
 
-
+    private void ContaryEnviar() {
        
-    
     }
-    
-    public void AgregarEntrada(String Busqueda,int Nparticion){
 
+    private void Crearfichero() {
+    PrintWriter writer = null;
+    try {
+        writer = new PrintWriter("ID "+ID+".txt", "UTF-8");
+    } catch (FileNotFoundException | UnsupportedEncodingException ex) {
+        Logger.getLogger(Hilo.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        
+    writer.print(SinStopWords);
+        
+    writer.close();
     }
     
 
